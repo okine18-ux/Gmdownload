4f1c5853acab8d6b591b4aac5e31707b84536516
okine18-ux <cpagamer18@gmail.com>
2025-11-25 00:11:55 +0100

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="mx-auto">
+        <div className="w-[100%] flex justify-center items-center h-[60px] bg-[#1d212e] text-[rgba(255,255,255,0.2)] footer-content">
+        </div>
+      </div>
+    </footer>
+  );
+};
